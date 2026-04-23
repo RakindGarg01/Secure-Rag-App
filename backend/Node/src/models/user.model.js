45e221b415f954981a786acbf01a96d209458b6c
Rakind Garg <rakindgarg@gmail.com>
2026-04-23 22:37:00 +0530

    Model Definiation for User

--- backend/Node/src/models/user.model.js
+++ backend/Node/src/models/user.model.js
@@ -1,7 +1,21 @@
 import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema({
-
+     "userName" : {
+          type : String,
+          required : true,
+          unique : true,
+          lowercase : true
+     },
+     "emailId" : {
+          type : String,
+          required : true,
+          unique : true
+     },
+     "password" : {
+          type : String,
+          required : true
+     }
 })
 
-export mongoose.model(User , userSchema)
+export const User = mongoose.model("User" , userSchema)
